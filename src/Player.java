4aea1328fe02b3de2f375c0c70884279d55ca2ac
Ataku-B <67343498+Ataku-B@users.noreply.github.com>
2020-12-16 00:19:35 +0300

    Server-Client base

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,44 @@
+import javax.swing.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Player {
+    private ClientSideConnection csc;
+    private int playerID;
+    private int otherPlayer;
+
+    public Player() {
+    }
+
+    public void connectToServer() {
+        csc = new ClientSideConnection();
+
+    }
+
+    //Client connection
+    private class ClientSideConnection {
+        private Socket socket;
+        private DataInputStream dataIn;
+        private DataOutputStream dataOut;
+
+        private ClientSideConnection() {
+            System.out.println("ClientSideConnection");
+            try {
+                socket = new Socket("localhost", 12345);
+                dataIn = new DataInputStream(socket.getInputStream());
+                dataOut = new DataOutputStream(socket.getOutputStream());
+                playerID = dataIn.readInt();
+                System.out.println("Connected to server as player #" + playerID);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Player p = new Player();
+        p.connectToServer();
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,83 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Server {
+
+    private ServerSocket ss;
+    private int numPlayers;
+    private int maxPlayers;
+    private ArrayList<ServerSideConnection> connections = new ArrayList<ServerSideConnection>();
+
+    public Server(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+        System.out.println("Server");
+        numPlayers = 0;
+        try {
+            ss = new ServerSocket(12345);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void acceptConnections() {
+        try {
+            System.out.println("Waiting for connections");
+            while( numPlayers < maxPlayers) {
+                Socket s = ss.accept();
+                numPlayers++;
+                System.out.println("Player # " + numPlayers + " has connected");
+                ServerSideConnection ssc = new ServerSideConnection(s, numPlayers);
+                connections.add(ssc);
+
+                Thread t = new Thread(ssc);
+                t.start();
+            }
+            System.out.println("We now have " + maxPlayers + " players");
+        } catch( IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ServerSideConnection implements Runnable {
+        private Socket socket;
+        private DataInputStream dataIn;
+        private DataOutputStream dataOut;
+        private int playerID;
+
+        public ServerSideConnection(Socket s, int id) {
+            socket = s;
+            playerID = id;
+            try {
+                dataIn = new DataInputStream(socket.getInputStream());
+                dataOut = new DataOutputStream(socket.getOutputStream());
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void run() {
+            try {
+                dataOut.writeInt(playerID);
+                dataOut.flush();
+
+
+                while(true) {
+
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(4);
+        server.acceptConnections();
+    }
 }
