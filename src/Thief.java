62d1f137af7473f03b5794488e7b56084a8c112e
Sarp Ulas Kaya <sarpkaya2000@gmail.com>
2020-12-16 21:33:42 +0300

    First Attempt at Thief's Move Method

--- src/Thief.java
+++ src/Thief.java
@@ -7,6 +7,10 @@ public class Thief extends Token
 
     public void move()
     {
-
+        super.move();
+        for (int i = 0; i < Game.instance.board.map[currentLocation].tokensOnTop.size(); i++ ) {
+            Token victim = Game.instance.tokens.get(Game.instance.board.map[currentLocation].tokensOnTop.get(i));
+            victim.payMoney(this,(victim.money / 100) * 5);
+        }
     }
 }
