20e7fbdaf2efd4387001f72c1e29d614848e4970
Furkan Baskaya <baskaya.furkan2000@gmail.com>
2020-12-16 12:23:48 +0300

    Implement Square class

---
+++ src/Square.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Square {
+    //variables
+    protected String name;
+    ArrayList<Integer> tokensOnTop;
+
+    //constructor
+    public Square(String name, ArrayList<Integer> tokensOnTop) {
+        this.name = name;
+        this.tokensOnTop = tokensOnTop;
+    }
+}
