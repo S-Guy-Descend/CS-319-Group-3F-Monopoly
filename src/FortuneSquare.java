71e55e53b4a987bfcd8933b2a9612d962c9fafa8
Furkan Baskaya <baskaya.furkan2000@gmail.com>
2020-12-16 14:55:49 +0300

    delete scroll_type constant

--- src/FortuneSquare.java
+++ src/FortuneSquare.java
@@ -1,5 +1,4 @@
 public class FortuneSquare extends Square {
-    final int SQUARE_TYPE = 2;
 
     public FortuneSquare(String name) {
         super(name);
--- src/ScrollSquare.java
+++ src/ScrollSquare.java
@@ -1,6 +1,5 @@
 public class ScrollSquare extends Square {
-    final int SQUARE_TYPE = 1; //Shows that square is a scroll square
-
+    
     public ScrollSquare(String name) {
         super(name);
     }
