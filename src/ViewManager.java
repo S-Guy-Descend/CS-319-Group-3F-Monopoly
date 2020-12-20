b039ed006b88c03afc8326e7909f465dcda96f58
Furkan Baskaya <baskaya.furkan2000@gmail.com>
2020-12-20 14:19:10 +0300

    update

--- src/ViewManager.java
+++ src/ViewManager.java
@@ -1,14 +1,9 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.geometry.Side;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
@@ -86,8 +81,9 @@ public class ViewManager {
         joinGameButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                LobbyViewManager lobby = new LobbyViewManager();
-                lobby.enterLobby();
+                GameViewManager lobby = new GameViewManager();
+                mainStage.hide();
+                lobby.enterGame();
             }
         });
 
